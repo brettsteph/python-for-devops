cbd34ccf453a056028771b6b4c2f7140924b5528
Brett Stephen <igadget777@gmail.com>
2023-03-31 02:40:58 +0000

    Add webhook

--- main.tf
+++ main.tf
@@ -60,3 +60,19 @@ resource "aws_codebuild_project" "example" {
     Environment = "Learning"
   }
 }
+
+resource "aws_codebuild_webhook" "example" {
+  project_name = aws_codebuild_project.example.name
+  build_type   = "BUILD"
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PUSH"
+    }
+
+    # filter {
+    #   type    = "BASE_REF"
+    #   pattern = "main"
+    # }
+  }
+}
