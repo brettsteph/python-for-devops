4d02eee4f8e0fc3f23c0d8b604a32e21d3ecaad1
Brett Stephen <igadget777@gmail.com>
2023-03-31 01:23:39 +0000

    Clean up

--- main.tf
+++ main.tf
@@ -12,103 +12,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-
-
-
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["codebuild.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+variable "github_token" {
+  type = string
 }
-
-resource "aws_iam_role" "example" {
-  name               = "example"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-data "aws_iam_policy_document" "example" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-
-    resources = ["*"]
-  }
-
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "ec2:CreateNetworkInterface",
-      "ec2:DescribeDhcpOptions",
-      "ec2:DescribeNetworkInterfaces",
-      "ec2:DeleteNetworkInterface",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeVpcs",
-    ]
-
-    resources = ["*"]
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["ec2:CreateNetworkInterfacePermission"]
-    resources = ["arn:aws:ec2:us-east-1:123456789012:network-interface/*"]
-
-    # condition {
-    #   test     = "StringEquals"
-    #   variable = "ec2:Subnet"
-
-    #   values = [
-    #     aws_subnet.example1.arn,
-    #     aws_subnet.example2.arn,
-    #   ]
-    # }
-
-    condition {
-      test     = "StringEquals"
-      variable = "ec2:AuthorizedService"
-      values   = ["codebuild.amazonaws.com"]
-    }
-  }
-
-  #   statement {
-  #     effect  = "Allow"
-  #     actions = ["s3:*"]
-  #     resources = [
-  #       aws_s3_bucket.example.arn,
-  #       "${aws_s3_bucket.example.arn}/*",
-  #     ]
-  #   }
-}
-
-resource "aws_iam_role_policy" "example" {
-  role   = aws_iam_role.example.name
-  policy = data.aws_iam_policy_document.example.json
+output "token" {
+  value = var.github_token
 }
 
 resource "aws_codebuild_source_credential" "example" {
   auth_type   = "PERSONAL_ACCESS_TOKEN"
   server_type = "GITHUB"
-  token       = "example"
+  token       = var.github_token
 }
 
 resource "aws_codebuild_project" "example" {
-  name = "test-project"
+  name = "fastapi-wiki-service"
   artifacts {
 
     type = "NO_ARTIFACTS"
@@ -132,7 +50,7 @@ resource "aws_codebuild_project" "example" {
     # }
   }
 
-  service_role = aws_iam_role.example.arn
+  service_role = "arn:aws:iam::247232402049:role/code-build-only-aws-service-role"
 
   source {
     type = "GITHUB"
@@ -165,10 +83,10 @@ resource "aws_codebuild_project" "example" {
   #     }
   #   }  
 
-  description   = "test_codebuild_project"
+  description   = "Build Wikipedia Service"
   build_timeout = "5"
 
-  source_version = "main"
+  # source_version = "main"
 
   #   vpc_config {
   #     vpc_id = aws_vpc.example.id
